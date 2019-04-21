7e435bb13d480a1cea5a9c6636c688faf1c9b4e0
League Student <league@iMac-8.attlocal.net>
2019-04-21 16:00:51 -0700

    maze
    mze

--- src/MazeMaker.java
+++ src/MazeMaker.java
@@ -15,23 +15,83 @@ public class MazeMaker{
 	
 	
 	public static Maze generateMaze(int w, int h){
+		Cell randomCell;
 		width = w;
 		height = h;
 		maze = new Maze(width, height);
 		
 		//select a random cell to start
-		
+		Random random= new Random();
+		int randomW = random.nextInt(w);
+		int randomH = random.nextInt(h);
+		randomCell = maze.getCell(randomW , randomH );
 		//call selectNextPath method with the randomly selected cell
-		
+		selectNextPath(randomCell);
 		return maze;
 	}
 
 	private static void selectNextPath(Cell currentCell) {
+		Cell rightCell;
+		Cell leftCell;
+		Cell downCell;
+		Cell upCell;
+		Random random = new Random();
+		ArrayList<Cell> unvisitedCells = new ArrayList<Cell>();
+		ArrayList<Cell> neighborCells = new ArrayList<Cell>();
+		Cell unvisitedCell;
 		// mark current cell as visited
-
+currentCell.setBeenVisited( true);
 		// check for unvisited neighbors
+if(currentCell.getX()<maze.getWidth()-1) {
+	rightCell = maze.getCell(currentCell.getX()+1, currentCell.getY());
+	neighborCells.add(rightCell);
+}
+if(currentCell.getX()>0) {
+	leftCell = maze.getCell(currentCell.getX()-1, currentCell.getY());
+	neighborCells.add(leftCell);
+}
+if(currentCell.getY()>0) {
+	upCell = maze.getCell(currentCell.getX(), currentCell.getY()-1);
+	neighborCells.add(upCell);
+}
+if(currentCell.getY()<maze.getHeight()-1) {
+	downCell = maze.getCell(currentCell.getX(), currentCell.getY()+1);
+	neighborCells.add(downCell);
+}
+		for(Cell neighbor:neighborCells) {
+			if(!neighbor.hasBeenVisited())
+			unvisitedCells.add(neighbor); 
+		}
 		
 		// if has unvisited neighbors,
+		if(unvisitedCells.size()>0) {
+			unvisitedCell = unvisitedCells.get(random.nextInt(unvisitedCells.size()));
+			uncheckedCells.push(unvisitedCell);
+			if(unvisitedCell.getX()>currentCell.getX()) {
+				currentCell.setEastWall(false);
+				unvisitedCell.setWestWall(false);
+			}
+			else if(unvisitedCell.getY()>currentCell.getY()) {
+				currentCell.setSouthWall(false);
+				unvisitedCell.setNorthWall(false);
+			}
+			else if(unvisitedCell.getX()<currentCell.getX()) {
+				currentCell.setWestWall(false);
+				unvisitedCell.setEastWall(false);
+			}
+			else{
+				currentCell.setNorthWall(false);
+				unvisitedCell.setSouthWall(false);
+			}
+				currentCell=unvisitedCell;
+				currentCell.setBeenVisited(true);
+				selectNextPath(currentCell);
+			
+		}if(!unvisitedCells.isEmpty()) {
+			currentCell = uncheckedCells.pop();
+			selectNextPath(currentCell);
+		}
+		
 			// select one at random.
 			// push it to the stack
 			// remove the wall between the two cells
